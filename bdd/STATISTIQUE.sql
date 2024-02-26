1ebbfa83dfa2ed60d5d74f8d206d60d31081d445
The0o <theolaidin@gmail.com>
2024-02-26 13:54:47 -0500

    Création de la table STATISTIQUE #4
    La table changera peut-être plus tard

---
+++ bdd/STATISTIQUE.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 26, 2024 at 06:33 PM
+-- Server version: 8.0.36-0ubuntu0.22.04.1
+-- PHP Version: 8.1.2-1ubuntu2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `monopoly`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `STATISTIQUE`
+--
+
+CREATE TABLE `STATISTIQUE` (
+  `nomUtilisateur` varchar(100) NOT NULL,
+  `nombreParties` int NOT NULL,
+  `nombreVictoire` int NOT NULL,
+  `nombreDefaite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `STATISTIQUE`
+--
+ALTER TABLE `STATISTIQUE`
+  ADD PRIMARY KEY (`nomUtilisateur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
